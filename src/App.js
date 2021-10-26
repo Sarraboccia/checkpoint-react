516d85ad7a75dc76ff7339fd1c7222aa555718ac
Sara Boccia <saraboccia@Saras-MacBook-Air.local>
2021-10-26 11:50:31 +0100

    add form from react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Form }from 'react-bootstrap'
 function App() {
   return (
     <div >
       <h1>Checkpoint react</h1>
-     
+      <>
+  <Form.Control size="lg" type="text" placeholder="Large text" />
+  <br />
+  <Form.Control type="text" placeholder="Normal text" />
+  <br />
+  <Form.Control size="sm" type="text" placeholder="Small text" />
+</>
     </div>
   );
 }
